efb6f23d55d7ef44351487f08ead9084af6de77c
Denis Bogomolov <denis.bogomolov@showmax.com>
2019-07-02 02:43:37 +0200

    Fix issues with symbols count

--- Sources/CodeInputView.swift
+++ Sources/CodeInputView.swift
@@ -7,37 +7,6 @@
 
 import UIKit
 
-/// Thanks to https://gist.github.com/preble/ab98fabda985b054126e
-class CodeInputTextStorage: NSTextStorage {
-
-    private var storage = NSTextStorage()
-
-    // MARK: NSTextStorage Primitive Methods
-    // https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/TextStorageLayer/Tasks/Subclassing.html
-
-    override var string: String {
-        return storage.string
-    }
-
-    override func attributes(at location: Int, effectiveRange range: NSRangePointer?) -> [NSAttributedString.Key : Any] {
-        return storage.attributes(at: location, effectiveRange: range)
-    }
-
-    override func replaceCharacters(in range: NSRange, with str: String) {
-        beginEditing()
-        storage.replaceCharacters(in: range, with: str)
-        edited(.editedCharacters, range: range, changeInLength: (str as NSString).length - range.length)
-        endEditing()
-    }
-
-    override func setAttributes(_ attrs: [NSAttributedString.Key : Any]?, range: NSRange) {
-        beginEditing()
-        storage.setAttributes(attrs, range: range)
-        edited(.editedAttributes, range: range, changeInLength: 0)
-        endEditing()
-    }
-}
-
 class CodeInputTextContainer: NSTextContainer {
     // swiftlint:disable:next line_length
     override func lineFragmentRect(forProposedRect proposedRect: CGRect, at characterIndex: Int, writingDirection baseWritingDirection: NSWritingDirection, remaining remainingRect: UnsafeMutablePointer<CGRect>?) -> CGRect {
@@ -63,12 +32,14 @@ open class CodeInputView: UITextView {
     public init(_ symbolsCount: Int, spacing: CGFloat) {
         self.symbolsCount = symbolsCount
         self.symbolSpacing = spacing
-        let textStorage = CodeInputTextStorage()
+        let textStorage = NSTextStorage()
         let layout = NSLayoutManager()
+        layout.showsInvisibleCharacters = true
         let container = CodeInputTextContainer(size: .zero)
         layout.addTextContainer(container)
         textStorage.addLayoutManager(layout)
         super.init(frame: .zero, textContainer: container)
+        delegate = self
         setup()
     }
 
@@ -154,5 +125,13 @@ open class CodeInputView: UITextView {
 
     open override func sizeToFit() {
         bounds.size = intrinsicContentSize
+    }
+}
+extension CodeInputView: UITextViewDelegate {
+    public func textViewDidChange(_ textView: UITextView) {
+        text = text.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        if text.count > symbolsCount {
+            text = String(text.prefix(symbolsCount))
+        }
     }
 }
